d84dfa3b1565302b18072b2e4b0175ecd2253ab6
Kevin Baribeau <kevin.baribeau@gmail.com>
2020-02-27 15:40:13 -0600

    Exercise 1: find the year a movie was released

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
   validates_presence_of :title
+
+  def self.year_released(title)
+    Movie.find_by(title: title)&.year
+  end
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -11,4 +11,18 @@ class MovieTest < ActiveSupport::TestCase
     movie = Movie.new(title: " ")
     assert_not movie.valid?
   end
+
+  test 'can find what year a movie was released' do
+    Movie.create(title: 'My Movie', year: '2020')
+
+    year = Movie.year_released('My Movie')
+
+    assert_equal '2020', year
+  end
+
+  test 'finding a year for a movie that doesnt exist returns nil' do
+    year = Movie.year_released('My Movie')
+
+    assert_nil year
+  end
 end
