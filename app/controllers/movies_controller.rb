54b1125616e10b1c66df3bacc39565132bd0411f
Kevin Baribeau <kevin.baribeau@gmail.com>
2020-02-27 16:41:19 -0600

    Challenge! Add form_for

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,27 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    new_movie = Movie.create(movie_form_params)
+
+    redirect_to movie_path(new_movie.id)
+  end
+
+  def update
+    Movie.find(params[:id]).update(movie_form_params)
+  end
+
+  private
+
+  def movie_form_params
+    params.require(:movie).permit(:title, :director, :plot_keywords, :facebook_likes, :year)
+  end
 end
