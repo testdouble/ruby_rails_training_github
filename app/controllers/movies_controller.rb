b27dce5fa1040a5cf72fca6fedbc53e921650a2e
Kevin Baribeau <kevin.baribeau@gmail.com>
2020-02-27 15:23:27 -0600

    Add routes, controllers, hard-coded view

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,4 @@
+class MoviesController < ApplicationController
+  def show
+  end
+end
--- test/system/movies_system_test.rb
+++ test/system/movies_system_test.rb
@@ -4,6 +4,12 @@ require "application_system_test_case"
 
 class MoviesSystemTest < ApplicationSystemTestCase
   test "visiting the show" do
-
+    # As a user,
+    # when I visit /movies/1
+    visit '/movies/1'
+    # I see the title of the movie "Parasite"
+    assert_text "Parasite"
+    # I also see the name of the director "Bong Joon-ho"
+    assert_text "Bong Joon-ho"
   end
 end
