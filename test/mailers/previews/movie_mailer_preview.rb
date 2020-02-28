132ba59f84f9f5fced937e6316ea3ed095db1bb0
Kevin Baribeau <kevin.baribeau@gmail.com>
2020-02-27 19:15:32 -0600

    Movie preview

--- test/mailers/previews/movie_mailer_preview.rb
+++ test/mailers/previews/movie_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/movie_mailer
 class MovieMailerPreview < ActionMailer::Preview
-
+  def new_movie_email
+    MovieMailer.with(movie: Movie.find(42)).new_movie_email
+  end
 end
