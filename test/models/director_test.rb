9c7307ee88d4a7e46ec9ddc07e76ab3c3b92f34e
Kevin Baribeau <kevin.baribeau@gmail.com>
2020-03-02 16:57:46 -0600

    Use Movies factory

--- test/models/director_test.rb
+++ test/models/director_test.rb
@@ -1,23 +1,14 @@
 # test/models/director_test.rb
+
 require 'test_helper'
 
 class DirectorTest < ActiveSupport::TestCase
   test "director has a name and age" do
     director = create(:director)
- 
-    # assert director.name,
-    # assert director.age, 50
+
     assert director.movies.count, 0
 
-    Movie.create(title: "Snowpiercer", facebook_likes: 70, plot_keywords: "snow|train", year: 2013, director: director)
-    Movie.create(title: "The Host", facebook_likes: 170, plot_keywords: "daughter|monster", year: 2006, director: director)
-    
-    # director.movies.create(title: "Snowpiercer", facebook_likes: 70, plot_keywords: "snow|train", year: 2013)
-    # director.movies.create(title: "The Host", facebook_likes: 170, plot_keywords: "daughter|monster", year: 2006)
-
-    # movie_1 = Movie.create(title: "Snowpiercer", facebook_likes: 70, plot_keywords: "snow|train", year: 2013)
-    # movie_2 = Movie.create(title: "The Host", facebook_likes: 170, plot_keywords: "daughter|monster", year: 2006)
-    # director.movies << [movie_1, movie_2]
+    movies = create_list(:movie, 3, director: director)
 
     assert director.movies.count, 2
   end
