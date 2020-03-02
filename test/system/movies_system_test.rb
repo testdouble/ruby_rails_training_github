072a2c17e4c9428d54b6f6c0bba3130ec271475d
Kevin Baribeau <kevin.baribeau@gmail.com>
2020-03-02 17:01:44 -0600

    Update view to use director.name; system test

--- test/system/movies_system_test.rb
+++ test/system/movies_system_test.rb
@@ -1,12 +1,10 @@
-# test/system/movies_system_test.rb
-
 require "application_system_test_case"
 
 class MoviesSystemTest < ApplicationSystemTestCase
   test "visiting the show" do
-    attributes = { title: "Parasite", director: 'Bong Joon-ho' }
+    director = Director.create(name: "Bong Joon-ho")
+    attributes = { title: "Parasite", director: director }
     movie = Movie.create(attributes)
-
     # As a user,
     # when I visit /movies/1
     visit "/movies/#{movie.id}"
@@ -17,12 +15,21 @@ class MoviesSystemTest < ApplicationSystemTestCase
   end
 
   test "visiting the show for another movie" do
-    attributes = { title: "Titanic", director: 'James Cameron' }
+    director = Director.create(name: "James Cameron")
+    attributes = { title: "Titanic", director: director }
     movie = Movie.create(attributes)
 
     visit "/movies/#{movie.id}"
-
     assert_text "Titanic"
     assert_text "James Cameron"
+  end
+
+  test "visiting the show for yet another movie" do
+    movie = create(:movie)
+
+    visit "/movies/#{movie.id}"
+
+    assert_text movie.title
+    assert_text movie.director.name
   end
 end
